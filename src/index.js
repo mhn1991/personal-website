526d5da2c117821ba9cf81bee0cdd5e56f84939e
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2022-03-21 00:05:25 +0000

    get rid of those garbage files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import leftSideBar from './leftSideBar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+	<leftSideBar />
+    </React.StrictMode>,
   document.getElementById('root')
 );
